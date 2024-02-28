32434532bd6a187789c8ff9037fa579a6370f7d7
peteykienzzz <pmatczak2@gmail.com>
2024-02-28 07:00:56 -0800

    added EC2 instance state resource to stop EC2 instance

--- aws/main.tf
+++ aws/main.tf
@@ -1,15 +1,25 @@
 # provider block 
-provider "awa" {
+provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_insrance" "app_server" {
+resource "aws_instance" "app_server" {
   ami = "" # choose which os you wasnt to use
   instance_type = "t2.micro"
   availability_zone = "us-west-2"
   key_name = "" # create a .pem keypair
 
-  tage = {
+  tags = {
     Name = "MyTerraformInstance"
   }
 }
+
+# Provides an EC2 instance state resource. This allows managing an instance power state.
+resource "aws_ec2_instance_state" "app_server" {
+  instance_id = aws_instance.app_server.id
+  state       = "stopped"
+}
+
+
+
+
